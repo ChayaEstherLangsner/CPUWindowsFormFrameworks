019289e29abd472d24f68e8c58d0be69997ba6ff
chayaestherlangsner <celangsner@gmail.com>
2023-09-10 14:17:38 -0400

    .

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace CPUWindowsFormFramework
 {
     public class WindowsFormsUtility
-    { 
+    {
         public static void SetListBinding(ComboBox lst, DataTable sourcedt, DataTable targetdt, string tablename)
         {
             lst.DataSource = sourcedt;
@@ -38,14 +39,61 @@ namespace CPUWindowsFormFramework
                 ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
         }
-            public static void FormatGridForSearchResults(DataGridView grid)
+        public static void FormatGridForSearchResults(DataGridView grid)
+        {
+            grid.AllowUserToAddRows = false;
+            grid.ReadOnly = true;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        public static bool IsFormOpen(Type formtype, int pkvalue = 0)
+        {
+            bool exists = false;
+            foreach (Form frm in Application.OpenForms)
             {
-                grid.AllowUserToAddRows = false;
-                grid.ReadOnly = true;
-                grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
-                grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                int frmpkvalue = 0;
+                if (frm.Tag != null && frm.Tag is int)
+                {
+                    frmpkvalue = (int)frm.Tag;
+                }
+                if (frm.GetType() == formtype && frmpkvalue == pkvalue)
+                {
+
+                    frm.Activate();
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
+        public static void SetUpNav(ToolStrip ts)
+        {
+            ts.Items.Clear();
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.IsMdiContainer == false)
+                {
+                    ToolStripButton btn = new();
+                    btn.Text = f.Text;
+                    btn.Tag = f;
+                    btn.Click += Btn_Click;
+                    ts.Items.Add(btn);
+                    ts.Items.Add(new ToolStripSeparator());
+                }
+            }
+        }
+        private static void Btn_Click(object? sender, EventArgs e)
+        {
+            if (sender != null && sender is ToolStripButton)
+            {
+                ToolStripButton btn = (ToolStripButton)sender;
+                if (btn.Tag != null && btn.Tag is Form)
+                {
+                    ((Form)btn.Tag).Activate();
+                }
             }
 
-        
+        }
     }
 }
